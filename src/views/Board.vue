663edc26ecb8538136e7abfa339cea4600a50562
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-02-27 17:54:03 +0100

    Finished board + added a task modal

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,12 +1,53 @@
 <template>
   <div class="board">
+    <div class="flex flex-row items-start">
+      <div class="column" v-for="(column, index) of board.columns" :key="index">
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div
+            class="task"
+            v-for="(task, index) of column.tasks"
+            :key="index"
+            @click="goToTask(task)"
+          >
+            <span class="w-full flex-no-shrink font-bold">{{ task.name }}</span>
+            <p
+              v-if="task.description"
+              class="w-full flex-no-shrink mt-1 text-sm"
+            >
+              {{ task.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 
+    <div class="task-bg" v-if="isTaskOpen" @click.self="close">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
 
+export default {
+  methods: {
+    goToTask (task) {
+      this.$router.push({ name: 'task', params: { id: task.id } })
+    },
+    close () {
+      this.$router.push({ name: 'board' })
+    }
+  },
+  computed: {
+    ...mapState(['board']),
+    isTaskOpen () {
+      return this.$route.name === 'task'
+    }
+  }
 }
 </script>
 
@@ -26,6 +67,6 @@ export default {
 
 .task-bg {
   @apply pin absolute;
-  background: rgba(0,0,0,0.5);
+  background: rgba(0, 0, 0, 0.5);
 }
 </style>
